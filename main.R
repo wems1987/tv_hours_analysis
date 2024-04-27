c84750e1d1e598e5e912a368d51e96d11b28d4ef
wems1987 <167352233+wems1987@users.noreply.github.com>
2024-04-27 08:37:47 +0000

    started analysis

---
+++ main.R
@@ -0,0 +1,9 @@
+
+library(tidyverse)
+library(here)
+
+tv_hours_table<- gss_cat%>%
+  group_by(marital)%>%
+  summarise(mean_tv_hours= mean(tvhours, na.rm = T))
+
+write_csv(tv_hours_table, here("TV_Hours_by_Marital.csv"))
